49321a9455e43734551ee9213b5e00b8b270212b
goku1088 <1023949048@teinco.edu.co>
2024-10-04 14:37:42 -0500

    Se coloco la forma a todas las preguntas y el random de categoria se agrego algo nuevo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-URL:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+function randomCategory() {
+    const categories = [
+        '../categorias/cienciadedatos.html',
+        '../categorias/astronomia.html',
+        '../categorias/historia.html',
+        '../categorias/deportes.html'
+    ];
+    const randomIndex = Math.floor(Math.random() * categories.length);
+    const categoria = document.querySelectorAll(".categorias")[randomIndex];
+    categoria.classList.add("seleccionada");
+    setTimeout(() => window.location.href = categories[randomIndex], 1000);
+}
