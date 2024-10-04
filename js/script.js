38b3ebe4d13ff9e0d74e0ac700ac5aef1c96d7ca
Angie1137 <1137089214@teinco.edu.co>
2024-10-03 20:59:55 -0500

    nuevos oambios categorias

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-URL:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+function randomCategory() {
+    const categories = [
+        '/preguntin/categorias/cat1.html',
+        '/preguntin/categorias/cat2.html',
+        '/preguntin/categorias/cat3.html',
+        '/preguntin/categorias/cat4.html'
+    ];
+    const randomIndex = Math.floor(Math.random() * categories.length);
+    console.log(randomIndex)
+    window.location.href = categories[randomIndex] 
+}
