cf40378c8c5c3043d9741d01985114ffc6f31763
Danx7c6b <1033703996@teinco.edu.co>
2024-11-02 14:21:46 -0500

    se realizo que al seleccionar la categoria esta mostrara una pregunta y al seleccionar la correcta esta pasara a la siguiente

--- js/script.js
+++ js/script.js
@@ -19,3 +19,44 @@ opcionesRespuesta.forEach((opcion, indice)=>{
     });
 });
 
+let respuestasCorrectas = 0;
+let respuestasIncorrectas = 0;
+
+document.getElementById('pregunta1').style.display = 'block';
+
+function mostrarPregunta(numero) {
+    const preguntas = document.querySelectorAll('.question');
+    preguntas.forEach((pregunta) => {
+        pregunta.style.display = 'none'; 
+    });
+    document.getElementById('pregunta' + numero).style.display = 'block'; 
+}
+
+function respuesta1(opcion) {
+    const resultadoDiv = document.getElementById('resultado');
+    if (opcion === 'Abner Doubleday') {
+        respuestasCorrectas++;
+        resultadoDiv.textContent = 'opcion correcta';
+        mostrarPregunta(2);
+    } else {
+        respuestasIncorrectas++;
+        resultadoDiv.textContent = 'opcion incorrecta'
+    }
+}
+
+function respuesta2(opcion) {
+    const resultadoDiv = document.getElementById('resultado');
+    if (opcion === '1896') {
+        respuestasCorrectas++;
+        resultadoDiv.textContent = 'opcion correcta'
+    } else {
+        respuestasIncorrectas++;
+        resultadoDiv.textContent = 'opcion incorrecta'
+    }
+    mostrarResultadosFinales();
+}
+
+function mostrarResultadosFinales() {
+    const resultadoDiv = document.getElementById('resultado');
+    resultadoDiv.textContent += `\n¡Cuestionario terminado! Respuestas correctas: ${respuestasCorrectas}, Respuestas incorrectas: ${respuestasIncorrectas}`;
+}
