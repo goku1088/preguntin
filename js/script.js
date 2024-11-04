a5df6b985277137b40b3c522c7cd1d67204fda79
Danx7c6b <1033703996@teinco.edu.co>
2024-11-04 15:46:36 -0500

    se realiza la parte del javascript con las modificaciones necesarias en el html para qu ele juego tenga las opciones que se requieren para el juego

--- js/script.js
+++ js/script.js
@@ -20,11 +20,7 @@ opcionesRespuesta.forEach((opcion, indice)=>{
 });
 
 let ordenPreguntas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-function obtenerPreguntasAleatorias(preguntas, cantidad) {
-    return preguntas.sort(() => Math.random() - 0.5).slice(0, cantidad);
-}
-
-ordenPreguntas = obtenerPreguntasAleatorias(ordenPreguntas, 5);
+ordenPreguntas = ordenPreguntas.sort(() => Math.random() - 0.5);
 let posicionActual = 0;
 let respuestasCorrectas = 0;
 let respuestasIncorrectas = 0;
@@ -44,7 +40,7 @@ function siguientePregunta() {
     if (posicionActual < ordenPreguntas.length) {
         mostrarPregunta(ordenPreguntas[posicionActual]);
     } else {
-        mostrarResultadoFinal();
+        mostrarResultadoFinal(); 
     }
 }
 
@@ -54,6 +50,7 @@ function actualizarVidas() {
     for (let i = 0; i < maxVidas; i++) {
         const heart = document.createElement('img');
         heart.src = '../img/heart-solid.svg';
+        heart.alt = 'icono de un corazón';
         if (i >= intentosRestantes) {
             heart.src = '../img/heart-regular.svg'; 
         }
@@ -67,64 +64,75 @@ function verificarRespuesta(correcta, opcion) {
         return true;
     } else {
         respuestasIncorrectas++;
-        intentosRestantes--;
+        intentosRestantes--; 
         actualizarVidas(); 
         if (intentosRestantes === 0) {
             mostrarResultadoFinal(); 
             return false;
         }
+        alert(`Respuesta incorrecta. Intentos restantes: ${intentosRestantes}`);
         return false;
     }
 }
 
-function respuesta1(opcion) {
-    verificarRespuesta('Abner Doubleday', opcion);
-    siguientePregunta();
+function respuesta1(opcion){
+    if (verificarRespuesta('Abner Doubleday', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function respuesta2(opcion) {
-    verificarRespuesta('1896', opcion);
-    siguientePregunta();
+    if (verificarRespuesta('1896', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function respuesta3(opcion) {
-    verificarRespuesta('Uruguay', opcion);
-    siguientePregunta();
+    if (verificarRespuesta('Uruguay', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function respuesta4(opcion) {
-    verificarRespuesta('LeBron James', opcion);
-    siguientePregunta();
+    if (verificarRespuesta('LeBron James', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function respuesta5(opcion) {
-    verificarRespuesta('Peñarol', opcion);
-    siguientePregunta();
+    if (verificarRespuesta('Peñarol', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function respuesta6(opcion) {
-    verificarRespuesta('Independiente', opcion);
-    siguientePregunta();
+    if (verificarRespuesta('Independiente', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function respuesta7(opcion) {
-    verificarRespuesta('Uruguay', opcion);
-    siguientePregunta();
+    if (verificarRespuesta('Uruguay', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function respuesta8(opcion) {
-    verificarRespuesta('Novak Djokovic', opcion);
-    siguientePregunta();
+    if (verificarRespuesta('Novak Djokovic', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function respuesta9(opcion) {
-    verificarRespuesta('1877', opcion);
-    siguientePregunta();
+    if (verificarRespuesta('1877', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function respuesta10(opcion) {
-    verificarRespuesta('1950', opcion);
-    siguientePregunta();
+    if (verificarRespuesta('1950', opcion)) {
+        siguientePregunta();
+    }
 }
 
 function mostrarResultadoFinal() {
@@ -134,20 +142,10 @@ function mostrarResultadoFinal() {
         <p>Respuestas correctas: ${respuestasCorrectas}</p>
         <p>Respuestas incorrectas: ${respuestasIncorrectas}</p>
         <p>Intentos restantes: ${intentosRestantes}</p>
-    `;
-
-    if (intentosRestantes === 0) {
-        const icono = document.createElement('img');
-        icono.src = '../img/no-intents.svg';
-        icono.alt = 'No quedan intentos';
-        icono.style.width = '50px';
-        icono.style.display = 'block';
-        resultadoDiv.appendChild(icono);
-    }
-}
+    `;}
 
 mostrarPregunta(ordenPreguntas[posicionActual]);
-actualizarVidas();
+actualizarVidas(); 
 
 
 
